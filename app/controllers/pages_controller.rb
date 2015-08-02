e4ba819254594b5ec6b727d7c69990802fcc4241
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-08-02 18:28:59 -0500

    Clean files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 class PagesController < ApplicationController
 	def home
 	end
-
-	def spahish_home
-	end
-
-	def final_home
-
-	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'english' => 'pages#home'
-  get 'new_version' => 'pages#final_home'
   root 'pages#home'
 end
