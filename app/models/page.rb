695f7b1c3bc384c6f0bc8cb5f4837ee41e7b678c
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-08-06 13:01:49 -0500

    Run annotate gem

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: pages
+#
+#  id         :integer          not null, primary key
+#  title1     :string
+#  text1      :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Page < ActiveRecord::Base
 end
