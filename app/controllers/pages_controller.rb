3ed046ae7302d2a79fec41f872f34028d2359e7d
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-08-06 12:40:30 -0500

    Add action to save the changes into page model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
 	def home
 		@page = Page.find(params[:id])
 	end
+
+	def save_page
+	  page = Page.find(params[:id])
+	  page.title1 = params[:content][:page_title1][:value]
+	  page.text1 = params[:content][:page_text1][:value]
+	  page.save!
+	  render text: ''
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   mount Mercury::Engine => '/'
   root to: 'pages#home', id: '1'
+  put '/', to: 'pages#save_page', id: '1'
 end
